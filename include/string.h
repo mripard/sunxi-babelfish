aeedae4b19bfeec4526a9868f092167ff5217b4a
Maxime Ripard <maxime.ripard@free-electrons.com>
2014-01-01 22:44:54 +0100

    Implement additional string.h function
    memchr, memcmp, memmove, memset and strchr are all needed by the libfdt.
    Provide some dumb implementation.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- include/string.h
+++ include/string.h
@@ -3,7 +3,13 @@
 
 #include <types.h>
 
+void *memchr(const void *s, int c, size_t n);
+int memcmp(const char *s1, const char *s2, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
+void *memset(void *s, int c, size_t n);
+
+char *strchr(const char *s, int c);
 int strcmp(const char *s1, const char *s2);
 size_t strlen(const char *s);
 
--- src/string.c
+++ src/string.c
@@ -1,6 +1,22 @@
 #include <string.h>
 #include <types.h>
 
+void *memchr(const void *s, int c, size_t n)
+{
+	const unsigned char *src = s;
+	unsigned char ch = c;
+
+	while ((*src++ != ch) && --n);
+
+	return n ? (void*)src : NULL;
+}
+
+int memcmp(const char *s1, const char *s2, size_t n)
+{
+	while ((*s1++ == *s2++) && --n);
+	return (*(unsigned char *)--s1 - *(unsigned char *)--s2);
+}
+
 void *memcpy(void *dest, const void *src, size_t n)
 {
 	const char *in = src;
@@ -11,6 +27,43 @@ void *memcpy(void *dest, const void *src, size_t n)
 		out[i] = in[i];
 
 	return dest;
+}
+
+void *memmove(void *dest, const void *src, size_t n)
+{
+	const char *in = src;
+	char *out = dest;
+	int i;
+
+	if (dest < src) {
+		for (i = 0; i < n; i++) {
+			out[i] = in[i];
+		}
+	} else if (dest > src) {
+		for (i = (n - 1); i >= 0; i--) {
+			out[i] = in[i];
+		}
+	}
+
+	return dest;
+}
+
+void *memset(void *s, int c, size_t n)
+{
+	char *src = s;
+	int i;
+
+	for (i = 0; i < n; i++)
+		src[i] = c;
+
+	return s;
+}
+
+char *strchr(const char *s, int c)
+{
+	while ((*s++ != c) && *s);
+
+	return (*--s == c) ? (void*)s : NULL;
 }
 
 int strcmp(const char *s1, const char *s2)
