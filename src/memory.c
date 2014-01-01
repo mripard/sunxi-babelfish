497b4e233c345a9827732d4467716d48357e2ed1
Maxime Ripard <maxime.ripard@free-electrons.com>
2014-01-01 18:45:37 +0100

    Align malloc sizes to 32bits
    If a size was passed that was not a multiple of 4, malloc was returning a
    non-aligned memory address the next time it was called, resulting in a data
    abort.
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- src/memory.c
+++ src/memory.c
@@ -1,12 +1,16 @@
 #include <types.h>
 
-void *malloc_base = 0x44000000;
+void *malloc_base = (void *)0x44000000;
 
 /* Dummy memory allocator */
 void *malloc(size_t size)
 {
 	void *ret = malloc_base;
-	malloc_base += size;
+	size_t aligned;
+
+	aligned = (((size - 1) / 4) + 1) * 4;
+
+	malloc_base += aligned;
 
 	return ret;
 }
