e2ebf79b07c41bdc086322d5eb526d329adcb990
Maxime Ripard <maxime.ripard@free-electrons.com>
2014-01-02 17:41:51 +0100

    Lookup in the FEX file for the default UART to output on
    And pass it in /chosen/bootargs
    
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- src/fixup.c
+++ src/fixup.c
@@ -1,12 +1,22 @@
 #include <fixup.h>
 #include <libfdt.h>
+#include <memory.h>
 #include <print.h>
+#include <string.h>
+
+#define CONSOLE_MAX_LEN		255
 
 int fdt_fixup(struct soc *soc, void *fdt, struct script *script)
 {
 	struct script_section *section;
-	struct script_string_entry *entry;
+	struct script_string_entry *str_entry;
+	struct script_single_entry *sword_entry;
+	char *bootargs;
 	int ret, offset;
+	char idx;
+
+	bootargs = malloc(CONSOLE_MAX_LEN);
+	strcpy(bootargs, "earlyprintk ");
 
 	/* Set machine compatible (offset 0 is the root node) */
 	ret = fdt_setprop_string(fdt, 0, "compatible", soc->compatible);
@@ -15,10 +25,19 @@ int fdt_fixup(struct soc *soc, void *fdt, struct script *script)
 
 	/* Set machine model (offset 0 is the root node) */
 	section = script_find_section(script, "product");
-	entry = (struct script_string_entry *)script_find_entry(section, "machine");
-	ret = fdt_setprop_string(fdt, 0, "model", entry->string);
+	str_entry = (struct script_string_entry *)script_find_entry(section, "machine");
+	ret = fdt_setprop_string(fdt, 0, "model", str_entry->string);
 	if (ret < 0)
 		return ret;
+
+	/* Set default UART to output on */
+	section = script_find_section(script, "uart_para");
+	sword_entry = (struct script_single_entry *)script_find_entry(section, "uart_debug_port");
+	idx = '0' + sword_entry->value;
+
+	strcat(bootargs, "console=ttyS");
+	strncat(bootargs, &idx, 1);
+	strcat(bootargs, ",115200 ");
 
 	/* Set bootargs */
 	offset = fdt_path_offset(fdt, "/chosen");
@@ -29,7 +48,7 @@ int fdt_fixup(struct soc *soc, void *fdt, struct script *script)
 			return offset;
 	}
 
-	ret = fdt_setprop_string(fdt, offset, "bootargs", "earlyprintk");
+	ret = fdt_setprop_string(fdt, offset, "bootargs", bootargs);
 	if (ret < 0)
 		return ret;
 
