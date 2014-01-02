5f67f1fc52ffd39ed08e5fea4661eaa0a03573fe
Maxime Ripard <maxime.ripard@free-electrons.com>
2014-01-02 13:49:08 +0100

    Parse the device tree and relocate it
    Signed-off-by: Maxime Ripard <maxime.ripard@free-electrons.com>

--- src/main.c
+++ src/main.c
@@ -4,9 +4,12 @@
 #include <types.h>
 #include <version.h>
 
+#include <libfdt.h>
+
 #include <lib/libfex/script_bin.h>
 
 #define SCRIPT_BASE_ADDRESS	(void*)0x43000000
+#define FDT_BASE_ADDRESS	(void*)0x45000000
 
 void main(u32 dummy, u32 machid, const struct tag *tags)
 	__attribute__((section(".text_main")));
@@ -35,4 +38,5 @@ void main(u32 dummy, u32 machid, const struct tag *tags)
 	if (!ret)
 		return;
 
+	ret = fdt_open_into(soc->fdt, FDT_BASE_ADDRESS, 2 * fdt_totalsize(soc->fdt));
 }
